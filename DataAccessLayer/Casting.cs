df8d8ecacbb56327d32ecf4329ec17e9ac2a3e51
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2024-05-23 16:20:30 +0300

    is continued

---
+++ DataAccessLayer/Casting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Casting
+    {
+        public int ID { get; set; }
+        public int PatternID { get; set; }
+        public string Pattern { get; set; }
+        public int ShiftID { get; set; }
+        public string Shift { get; set; }
+        public DateTime CastingDate { get; set; }
+        public int ProductID { get; set; }
+        public string Product { get; set; }
+    }
+}
---
+++ DataAccessLayer/Code.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Code
+    {
+        public int ID { get; set; }
+        public Int16 Number { get; set; }
+        public string Definition { get; set; }
+        public string Mud { get; set; }
+        public string PatternCode { get; set; }
+        public string Description { get; set; }
+        public string StockCode { get; set; }
+        public int Stock { get; set; }
+        public string Group { get; set; }
+        public int Stock { get; set; }
+        public int Stock { get; set; }
+        public int Stock { get; set; }
+        public int Stock { get; set; }
+    }
+}
