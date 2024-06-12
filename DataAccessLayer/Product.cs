92a4512b39f5f9ae5830768e2f6b75528fa7cb3e
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2024-06-12 09:50:57 +0300

    4

--- DataAccessLayer/Product.cs
+++ DataAccessLayer/Product.cs
@@ -30,6 +30,11 @@ namespace DataAccessLayer
         public string Definition { get; set; }
         public Int16 Quality { get; set; }
         public int Count { get; set; }
+        public int Count1 { get; set; }
+        public int Count2 { get; set; }
+        public int Count3 { get; set; }
+        public int Count4 { get; set; }
+        public int Count5 { get; set; }
         public string Code { get; set; }
         public string RetouchFire { get; set; }
         public int SelectedMonth { get; set; }
---
+++ PersonalControl/detail_form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonalControl
+{
+    public partial class detail_form : Form
+    {
+        public detail_form()
+        {
+            InitializeComponent();
+        }
+
+        private void detail_form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
